64d2de5b00a06ae8a0c4d64cdb7de5e349091bfb
Sam A <sam.arnerich@hotmail.co.nz>
2026-02-21 23:25:06 +1300

    Work on backend for sign up page
    #DB MIGRATION - Add account_enabled column to users table

--- components/ui/signup/signup-form.tsx
+++ components/ui/signup/signup-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -15,11 +17,15 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useActionState } from "react";
+import { signup } from "@/lib/signup";
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [state, action, pending] = useActionState(signup, undefined);
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -30,32 +36,71 @@ export function SignupForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form action={action}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="name">Name</FieldLabel>
-                <Input id="name" type="text" placeholder="John Doe" required />
+                <Input
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="John Doe"
+                  required
+                />
+                {state?.errors?.name && <p>{state.errors.name}</p>}
               </Field>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
                 <Input
                   id="email"
                   type="email"
+                  name="email"
                   placeholder="m@example.com"
                   required
                 />
+                {state?.errors?.email && <p>{state.errors.email}</p>}
               </Field>
               <Field>
                 <Field className="grid grid-cols-2 gap-4">
                   <Field>
                     <FieldLabel htmlFor="password">Password</FieldLabel>
-                    <Input id="password" type="password" required />
+                    <Input
+                      id="password"
+                      type="password"
+                      name="password"
+                      required
+                    />
+                    {state?.errors?.password && (
+                      <div>
+                        <p>Password must:</p>
+                        <ul>
+                          {state.errors.password.map((error) => (
+                            <li key={error}>- {error}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
                   </Field>
                   <Field>
                     <FieldLabel htmlFor="confirm-password">
                       Confirm Password
                     </FieldLabel>
-                    <Input id="confirm-password" type="password" required />
+                    <Input
+                      id="confirm-password"
+                      type="password"
+                      name="confirm-password"
+                      required
+                    />
+                    {state?.errors?.confirmPassword && (
+                      <div>
+                        <p>Password must:</p>
+                        <ul>
+                          {state.errors.confirmPassword.map((error) => (
+                            <li key={error}>- {error}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
                   </Field>
                 </Field>
                 <FieldDescription>
@@ -63,7 +108,9 @@ export function SignupForm({
                 </FieldDescription>
               </Field>
               <Field>
-                <Button type="submit">Create Account</Button>
+                <Button disabled={pending} type="submit">
+                  Create Account
+                </Button>
                 <FieldDescription className="text-center">
                   Already have an account? <Link href="/login">Sign in</Link>
                 </FieldDescription>
